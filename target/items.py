24fe639ac90ba0476e64a05b50d727153f4400f1
taras-tverdokhlib <57938979+taras-tverdokhlib@users.noreply.github.com>
2022-06-11 15:04:02 +0300

    items created

--- target/items.py
+++ target/items.py
@@ -3,10 +3,17 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from pandas import describe_option
 import scrapy
 
 
 class TargetItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    highlights = scrapy.Field()
+    last_question = scrapy.Field()
+    last_answer = scrapy.Field()
     pass
