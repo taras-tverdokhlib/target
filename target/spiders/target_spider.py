c040ecf4ee4e7afddd8ad6a9a03cbef44e3b2e08
taras-tverdokhlib <57938979+taras-tverdokhlib@users.noreply.github.com>
2022-06-14 09:31:10 +0300

    added proxy string and images list comperhensions

--- target/spiders/target_spider.py
+++ target/spiders/target_spider.py
@@ -9,6 +9,7 @@ class TargetSpider(scrapy.Spider):
     def start_requests(self):
         link = 'https://www.target.com/p/tracfone-prepaid-apple-iphone-se-2nd-gen-64gb-black/-/A-82040163#lnk=sametab'
         request = scrapy.Request(url = link, callback = self.parse)
+        #request.meta['proxy'] = ''
         yield request
 
     def parse(self, response):
@@ -18,9 +19,7 @@ class TargetSpider(scrapy.Spider):
         #item['price'] = response.xpath('/html/body/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div[1]/span').extract()
         item['images'] = []
         images = response.xpath('//@src').extract()
-        for image in images:
-            if image.endswith('pjpeg') and 'wid=800' in image:
-                item['images'].append(image)
+        item['images'] = [image for image in images if (image.endswith('pjpeg') and 'wid=800' in image)]
         item['description'] = response.xpath('//*[@id="specAndDescript"]/div[1]/div[2]/div//text()').get()
         item['highlights'] =  response.xpath('//*[@id="tabContent-tab-Details"]/div/div/div/div[1]/div/div/ul//text()').extract()
         # item['last_question'] = 
